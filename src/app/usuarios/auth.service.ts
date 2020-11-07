79903eb6a5a84061a79aea411b29ee9b7ed31f09
Jimmy Valdez <jimmyvb16@gmail.com>
2020-11-07 11:14:19 -0500

    SessionStorage y Error user/password
    Se guardan los datos y token del usuario en el sessionStorage, y se esta validando el usuario y password mostrando un alert

--- src/app/usuarios/auth.service.ts
+++ src/app/usuarios/auth.service.ts
@@ -8,8 +8,31 @@ import { Usuario } from './usuario';
 })
 export class AuthService {
 
+  private _usuario: Usuario;
+  private _token: string;
+
   //Inyectamos el HttpClient en el constructor
   constructor(private http: HttpClient) { }
+
+  public get usuario(): Usuario{
+    if(this._usuario != null){
+      return this._usuario;
+    } else if(this._usuario == null && sessionStorage.getItem('usuario') != null){
+      this._usuario = JSON.parse(sessionStorage.getItem('usuario')) as Usuario;
+      return this._usuario;
+    }
+    return new Usuario();
+  }
+
+  public get token(): string{
+    if(this._token != null){
+      return this._token;
+    } else if(this._token == null && sessionStorage.getItem('token') != null){
+      this._token = sessionStorage.getItem('usuario');
+      return this._token;
+    }
+    return null;
+  }
 
   login(usuario: Usuario): Observable<any>{
     const urlEndpoint = 'http://localhost:8080/oauth/token';
@@ -25,5 +48,28 @@ export class AuthService {
     params.set('password', usuario.password);
 
     return this.http.post(urlEndpoint, params.toString(), {headers: httpHeaders});
+  }
+
+  guardarUsuario(accessToken: string):void{
+    let payload = this.obtenerDatosToken(accessToken);
+    this._usuario = new Usuario();
+    this._usuario.nombre = payload.nombre;
+    this._usuario.apellido = payload.apellido
+    this._usuario.email = payload.email;
+    this._usuario.username = payload.user_name;
+    this._usuario.roles = payload.authorities;
+    sessionStorage.setItem('usuario', JSON.stringify(this._usuario));//Convierte un objeto en string
+  }
+  
+  guardarToken(accessToken: string):void{
+    this._token = accessToken;
+    sessionStorage.setItem('token', accessToken);
+  }
+
+  obtenerDatosToken(accessToken: string):any{
+    if(accessToken != null ){
+      return JSON.parse(atob(accessToken.split(".")[1]));
+    }
+    return null;
   }
 }
