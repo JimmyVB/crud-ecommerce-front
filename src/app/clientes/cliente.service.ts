2d630d720a352d4ff286a39f662b80ac5ad1d356
Jimmy Valdez <jimmyvb16@gmail.com>
2020-11-11 21:05:00 -0500

    HTTP Interceptor 401 y 403 Refactor
    HttpInterceptor para códigos HTTP 401 y 403

--- src/app/clientes/cliente.service.ts
+++ src/app/clientes/cliente.service.ts
@@ -1,13 +1,11 @@
 import { Injectable } from '@angular/core';
 import { Cliente } from './cliente';
 import { Observable, of, throwError } from 'rxjs';
-import { HttpClient, HttpEvent, HttpHeaders, HttpRequest } from '@angular/common/http';
+import { HttpClient, HttpEvent, HttpRequest } from '@angular/common/http';
 import { map, catchError, tap } from 'rxjs/operators';
-import Swal from 'sweetalert2';
 import { Router } from '@angular/router';
 import { DatePipe } from '@angular/common';
 import { Region } from './region';
-import { AuthService } from '../usuarios/auth.service';
 
 @Injectable({
   providedIn: 'root'
@@ -15,33 +13,10 @@ import { AuthService } from '../usuarios/auth.service';
 export class ClienteService {
   private urlEndPoint: string = 'http://localhost:8080/api/clientes';
 
-  constructor(private http: HttpClient, private router: Router, private authService: AuthService) { }
-
-  private isNoAutorizado(e): boolean {
-    //401 = No autorizado y 403 = Prohibido
-    if(e.status == 401){
-
-      if(this.authService.isAuthenticated()){
-        this.authService.logout();
-      }
-      this.router.navigate(['/login']);
-      return true;
-    }
-    if(e.status == 403){
-      Swal.fire('Accesso denegado', `Hola ${this.authService.usuario.username} no tienes acceso a este recurso`, 'warning');
-      this.router.navigate(['/clientes']);
-      return true;
-    }
-    return false;
-  }
+  constructor(private http: HttpClient, private router: Router) { }
 
   getRegiones(): Observable<Region[]>{
-    return this.http.get<Region[]>(this.urlEndPoint + '/regiones').pipe(
-      catchError(e => {
-        this.isNoAutorizado(e);
-        return throwError(e);
-      })
-    );
+    return this.http.get<Region[]>(this.urlEndPoint + '/regiones');
   }
 
   getClientes(page: number): Observable<any> { 
@@ -78,15 +53,12 @@ export class ClienteService {
       map( (response:any) => response.cliente as Cliente),
       catchError(e => {
 
-        if(this.isNoAutorizado(e)){
-          return throwError(e);
-        }
-
         if(e.status == 400){
           return throwError(e);
         }
-        console.error(e.error.mensaje);
-        Swal.fire(e.error.mensaje, e.error.error, 'error');
+        if(e.error.mensaje){
+          console.error(e.error.mensaje);
+        }
         return throwError(e);
       })
     );
@@ -95,14 +67,12 @@ export class ClienteService {
   getCliente(id): Observable<Cliente> { 
     return this.http.get<Cliente>(`${this.urlEndPoint}/${id}`).pipe(
       catchError(e => {
-
-        if(this.isNoAutorizado(e)){
-          return throwError(e);
+   
+        if(e.status != 401 && e.error.mensaje){
+          this.router.navigate(['/clientes']);
+          console.error(e.error.mensaje);
         }
-        
-        this.router.navigate(['/clientes']);
-        console.error(e.error.mensaje);
-        Swal.fire('Error al editar', e.error.mensaje, 'error');
+
         return throwError(e);
       })
     );
@@ -112,15 +82,12 @@ export class ClienteService {
     return this.http.put<any>(`${this.urlEndPoint}/${cliente.id}`, cliente).pipe(
       catchError(e => {
 
-        if(this.isNoAutorizado(e)){
-          return throwError(e);
-        }
-
         if(e.status == 400){
           return throwError(e);
         }
-        console.error(e.error.mensaje);
-        Swal.fire(e.error.mensaje, e.error.error, 'error');
+        if(e.error.mensaje){
+          console.error(e.error.mensaje);
+        }
         return throwError(e);
       })
     );
@@ -129,13 +96,9 @@ export class ClienteService {
   delete(id: number): Observable<Cliente>{
     return this.http.delete<Cliente>(`${this.urlEndPoint}/${id}`).pipe(
       catchError(e => {
-
-        if(this.isNoAutorizado(e)){
-          return throwError(e);
+        if(e.error.mensaje){
+          console.error(e.error.mensaje);
         }
-
-        console.error(e.error.mensaje);
-        Swal.fire(e.error.mensaje, e.error.error, 'error');
         return throwError(e);
       })
     );
@@ -150,11 +113,6 @@ export class ClienteService {
       reportProgress: true
     });
     
-    return this.http.request(req).pipe(
-      catchError(e => {
-        this.isNoAutorizado(e);
-        return throwError(e);
-      })
-    );
+    return this.http.request(req);
   }
 }
