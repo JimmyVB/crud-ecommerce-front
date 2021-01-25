34373a3bf1e8afe09a99161dec390dc6e51c86b4
Jimmy Valdez <jimmyvb16@gmail.com>
2021-01-25 12:58:02 -0500

    Validando factura
    Se valido la factura desdel el input

--- src/app/facturas/facturas.component.ts
+++ src/app/facturas/facturas.component.ts
@@ -113,11 +113,18 @@ export class FacturasComponent implements OnInit {
     id !== item.producto.id);
   }
 
-  create(): void{
+  create(facturaForm): void{
     console.log(this.factura);
-    this.facturaService.create(this.factura).subscribe(factura => {
-      Swal.fire(this.titulo, `Factura ${factura.descripcion} creada con exito!`, 'success');
-      this.router.navigate(['/clientes']);
-    });
+
+    if(this.factura.items.length == 0){
+      this.autocompleteControl.setErrors({'invalid':true});
+    }
+
+    if(facturaForm.form.valid && this.factura.items.length > 0){
+      this.facturaService.create(this.factura).subscribe(factura => {
+        Swal.fire(this.titulo, `Factura ${factura.descripcion} creada con exito!`, 'success');
+        this.router.navigate(['/clientes']);
+      });
+    }
   }
 }
